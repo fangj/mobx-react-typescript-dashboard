577417ba255f892bf451fc6006f683004d73e47c
fangj <fangjianmail@126.com>
2017-06-09 14:18:58 +0800

    add loading animate

--- client/src/components/todo/TodoAppView.tsx
+++ client/src/components/todo/TodoAppView.tsx
@@ -5,11 +5,10 @@
 import * as React from "react";
 import {observer} from "mobx-react";
 import SummaryView from "./SummaryView";
-import ListView from "./TodoListView";
+import TodoListView from "./TodoListView";
 import TodoListStore from "../../stores/TodoListStore";
 import DevTools from "mobx-react-devtools";
 import * as styles from './style.css';
-import * as cx from 'classnames';
 
 interface ITodoAppView {
     todoListStore: TodoListStore
@@ -26,7 +25,7 @@ export default class TodoAppView extends React.Component<ITodoAppView, {}> {
         const todoListStore = this.props.todoListStore;
         return <div className={styles.verticalContainer}>
             <div className={styles.todoContainer}>
-                <ListView todoStores={todoListStore.todoStores}/>
+                <TodoListView todoStores={todoListStore.todoStores}/>
                 <SummaryView todoListStore={todoListStore}/>
                 <DevTools />
             </div>
